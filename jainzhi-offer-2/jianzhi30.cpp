d0bd4c6792f6427a3ab63f51caca4504796acc71
wczzsdsj <71573983+wczzsdsj@users.noreply.github.com>
2024-11-25 17:36:16 +0800

    t30 最小栈 不使用辅助空间

--- jainzhi-offer-2/jianzhi30.cpp
+++ jainzhi-offer-2/jianzhi30.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 #include<stack>
 using namespace std;
-
+//包含最小值的栈
 class MinStack
 {
 public:
@@ -41,6 +41,48 @@ public:
     int getMin()
     {
         return minst.top();
+    }
+};
+//不使用辅助空间
+class MinStack {
+public:
+    //s_min也需要为ll
+    ll s_min;
+    stack<ll> s;
+    MinStack() {
+        s_min=INT64_MAX;
+    }
+    
+    void push(int val) {
+        //当val为首个元素时，特殊处理，栈存放0
+        if(s.empty()==1){
+            s_min=val;
+            s.push(0);
+        }else{
+            ll sub=val-s_min;
+            if(sub<0){
+                s_min=val;
+            }
+            s.push(sub);
+        }
+    }
+    
+    void pop() {
+        if(s.top()<0){
+            s_min=s_min-s.top();
+        }
+        s.pop();
+    }
+    
+    int top() {
+        if(s.top()<0){
+            return s_min;
+        }
+        return s_min+s.top();
+    }
+    
+    int getMin() {
+        return s_min;
     }
 };
 int main(){
