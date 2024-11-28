e4887b00ec2e4caca888deb96e176bf2869a89cd
dsj <1315577081@qq.com>
2024-11-28 14:06:06 +0800

    update

--- jainzhi-offer-2/jianzhi28.cpp
+++ jainzhi-offer-2/jianzhi28.cpp
@@ -6,9 +6,9 @@ struct TreeNode{
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() : val(0), left(nullptr), right(nullptr) {};
-    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {};
-    TreeNode(int x, TreeNode *_left, TreeNode *_right) : val(x), left(_left), right(_right) {};
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *_left, TreeNode *_right) : val(x), left(_left), right(_right) {}
 };
 //二叉树是否对称
 class Solution{
--- jainzhi-offer-2/jianzhi34.cpp
+++ jainzhi-offer-2/jianzhi34.cpp
@@ -7,9 +7,9 @@ struct TreeNode{
     int val;
     TreeNode *left;
     TreeNode *right;
-    TreeNode() {};
-    TreeNode(int val) :val(val),left(nullptr),right(right) {};
-    TreeNode(int _val, TreeNode *_left, TreeNode *_right) : val(_val), left(_left), right(_right) {};
+    TreeNode() {}
+    TreeNode(int val) : val(val), left(nullptr), right(nullptr) {}
+    TreeNode(int _val, TreeNode *_left, TreeNode *_right) : val(_val), left(_left), right(_right) {}
 };
 // 二叉树中和为某一值的路径
 class Solution{
@@ -36,3 +36,16 @@ public:
         return ans;
     }
 };
+
+int main(){
+    TreeNode *left = new TreeNode(5);//堆上创建，需要delete
+    TreeNode *right = new TreeNode(6);
+    TreeNode node1;//栈上创建，不需要delete
+    TreeNode node2(5);
+    TreeNode node3(5,left,right);
+    printf("%d,%d,%d",node3.val,node3.left->val,node3.right->val);
+
+    delete left;
+    delete right;
+    return 0;
+}
