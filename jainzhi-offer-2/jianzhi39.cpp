b7570b04512dae60b6fc8acb8e64f267dcebf6b0
dsj <1315577081@qq.com>
2024-12-02 19:11:19 +0800

    t39 数组中出现次数超过一半的数字 快速排序法

--- jainzhi-offer-2/jianzhi39.cpp
+++ jainzhi-offer-2/jianzhi39.cpp
@@ -22,3 +22,35 @@ public:
         return major;
     }
 };
+
+//快速排序
+class Solution
+{
+public:
+    void quicksort(vector<int>& nums,int l,int r,int target){
+        if(l>=r)
+            return ;
+        int pivot = nums[r];
+        int left = l, right = l;
+        while(right<r){
+            if(nums[right]<pivot){
+                swap(nums[left],nums[right]);
+                left++;
+            }
+            right++;
+        }
+        swap(nums[left],nums[r]);
+        if(left==target)
+            return;
+        if(left>target)
+            quicksort(nums,l,left-1,target);
+        else
+           quicksort(nums,left+1,r,target);
+    }
+
+    int majorityElement(vector<int> &nums)
+    {
+        quicksort(nums,0,nums.size()-1,nums.size()/2);
+        return nums[nums.size() / 2];
+    }
+};
