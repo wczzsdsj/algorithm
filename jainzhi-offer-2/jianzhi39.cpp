5d734559b7ca0930565b2e97a09474d6321a9263
dsj <1315577081@qq.com>
2024-12-02 14:25:21 +0800

    t39 数组中出现次数超过一半的数字

---
+++ jainzhi-offer-2/jianzhi39.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+// 数组中出现次数超过一半的数字
+class Solution
+{
+public:
+    int majorityElement(vector<int> &nums)
+    {
+        int cnt = 0;
+        int major = 0;
+        for (int i = 0; i < nums.size();i++)
+        {
+            if(cnt==0)
+                major = nums[i];
+            if(nums[i]==major)
+                cnt++;
+            else
+                cnt--;
+        }
+        return major;
+    }
+};
