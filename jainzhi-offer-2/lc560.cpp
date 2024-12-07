73e2e7dbccf492ed8243044b44003d8c8af7353b
dsj <1315577081@qq.com>
2024-12-08 00:21:22 +0800

    lc560 和为 K 的子数组

--- jainzhi-offer-2/lc560.cpp
+++ jainzhi-offer-2/lc560.cpp
@@ -29,3 +29,30 @@ public:
         return ans;
     }
 };
+
+class Solution
+{
+public:
+    int subarraySum(vector<int> &nums, int k)
+    {
+        if (nums.size() == 0)
+            return 0;
+        int n = nums.size();
+        int prefix[n];
+        int ans = 0;
+        unordered_map<int, int> mp;
+        mp[0] = 1;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (i == 0)
+                prefix[0] = nums[0];
+            else
+                prefix[i] = prefix[i - 1] + nums[i];
+            ans += mp[prefix[i] - k];
+            mp[prefix[i]]++;
+        }
+
+        return ans;
+    }
+};
