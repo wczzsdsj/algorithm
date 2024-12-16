f0852790cde16436d053a51460c2413f46082558
dsj <1315577081@qq.com>
2024-12-16 14:31:20 +0800

    t53-1 在排序数组中查找数字 I

--- jainzhi-offer-2/jianzhi53-1.cpp
+++ jainzhi-offer-2/jianzhi53-1.cpp
@@ -52,3 +52,29 @@ class Solution {
         return {firstindex,lastindex};
     }
 };
+
+class Solution {
+  public:
+    int binarysearcch(vector<int> &nums, int target,int flag) {
+        int i = 0, j = nums.size() - 1;
+        int res = -1;
+        while(i<=j){
+            int mid = (i + j) / 2;
+            if(nums[mid]>target){
+                j = mid - 1;
+            }else if(nums[mid]<target){
+                i = mid + 1;
+            }else{
+                res = mid;
+                if(flag)
+                    i = mid + 1;
+                else
+                    j = mid - 1;
+            }
+        }
+        return res;
+    }
+    vector<int> searchRange(vector<int> &nums, int target) {
+        return {binarysearcch(nums, target, 0), binarysearcch(nums, target, 1)};
+    }
+};
