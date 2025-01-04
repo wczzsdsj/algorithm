c022cb5fad97fdf38919ea198397ff3c8e048e9b
dsj <1315577081@qq.com>
2025-01-04 17:12:31 +0800

    hot025 环形链表

---
+++ hot100/hot025-lc141.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// 141. 环形链表
+class Solution {
+  public:
+    bool hasCycle(ListNode *head) {
+        ListNode *fast = head, *slow = head;
+        while (fast && fast->next) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow)
+                return true;
+        }
+        return false;
+    }
+};
