848f4b79513e36330254ecaba6d9470fb3824c4e
dsj <1315577081@qq.com>
2025-01-08 13:39:44 +0800

    hot038 翻转二叉树

---
+++ hot100/hot038-lc226.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+// 226. 翻转二叉树
+class Solution {
+  public:
+    TreeNode *invertTree(TreeNode *root) {
+        if (root == nullptr)
+            return root;
+        TreeNode *left = invertTree(root->left);
+        TreeNode *right = invertTree(root->right);
+        root->left = right;
+        root->right = left;
+        return root;
+    }
+};
