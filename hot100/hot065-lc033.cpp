d50a811ce4141419e74d4752c8ab8e5f9b0f10ec
dsj <1315577081@qq.com>
2025-01-15 21:37:25 +0800

    hot065 搜索旋转排序数组(有无重复)

--- hot100/hot065-lc033.cpp
+++ hot100/hot065-lc033.cpp
@@ -11,18 +11,47 @@ class Solution {
             if (nums[mid] == target)
                 return mid;
             else if (nums[mid] >= nums[0]) {
-                if (target >= nums[0] && target < nums[mid]) {
+                if (target >= nums[l] && target < nums[mid]) {
                     r = mid - 1;
                 } else {
                     l = mid + 1;
                 }
             } else {
-                if (target > nums[mid] && target <= nums[nums.size() - 1]) {
+                if (target > nums[mid] && target <= nums[r]) {
                     l = mid + 1;
                 } else
                     r = mid - 1;
             }
         }
         return -1;
+    }
+};
+
+// 81. 搜索旋转排序数组 II （nums有重复的数字）
+class Solution {
+  public:
+    bool search(vector<int> &nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] == target)
+                return true;
+            if (nums[mid] == nums[l] && nums[mid] == nums[r]) {
+                l++;
+                r--;
+            } else if (nums[mid] >= nums[l]) {
+                if (target >= nums[l] && target < nums[mid]) {
+                    r = mid - 1;
+                } else {
+                    l = mid + 1;
+                }
+            } else {
+                if (target > nums[mid] && target <= nums[r]) {
+                    l = mid + 1;
+                } else
+                    r = mid - 1;
+            }
+        }
+        return false;
     }
 };
