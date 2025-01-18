4330f83e83f4b10e84b6ca4e50d4bd4cd7f860af
dsj <1315577081@qq.com>
2025-01-18 20:12:50 +0800

    hot074 前 K 个高频元素

--- hot100/hot074-lc347.cpp
+++ hot100/hot074-lc347.cpp
@@ -36,3 +36,28 @@ class Solution {
         return ret;
     }
 };
+
+// pair能直接比较大小
+class Solution {
+  public:
+    vector<int> topKFrequent(vector<int> &nums, int k) {
+        unordered_map<int, int> cnt;
+        using pii = pair<int, int>;
+        for (int x : nums) {
+            ++cnt[x];
+        }
+        priority_queue<pii, vector<pii>, greater<pii>> pq;
+        for (auto &[x, c] : cnt) {
+            pq.push({c, x});
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+        vector<int> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
+        }
+        return ans;
+    }
+};
