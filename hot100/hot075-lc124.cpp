a86aa1bd50ea27e8ae662c04f6cd71a19aed6aab
dsj <1315577081@qq.com>
2025-01-19 23:53:22 +0800

    hot075 二叉树中的最大路径和

---
+++ hot100/hot075-lc124.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+// 124. 二叉树中的最大路径和
+class Solution {
+  public:
+    int maxPathSum(TreeNode *root) {
+        int ret = INT_MIN;
+        // 这里不能用auto，因为函数内部调用了自身
+        function<int(TreeNode *)> dfs = [&](TreeNode *cur) -> int {
+            if (cur == nullptr)
+                return 0;
+            int curmaxsum = cur->val;
+            int l = dfs(cur->left);
+            int r = dfs(cur->right);
+            if (l < 0)
+                l = 0;
+            if (r < 0)
+                r = 0;
+            // 左右经过cur节点的最大路径和
+            ret = max(ret, curmaxsum + l + r);
+            // 注意这里返回值，向上返回，需经过cur，所以加cur->val.到这一步l,r都>=0，向上返回上下经过cur节点的最大路径和
+            return cur->val + max(l, r);
+        };
+        dfs(root);
+        return ret;
+    }
+};
