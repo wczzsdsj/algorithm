4554b87aab2ba67655cc7f0124ce51dedc661d0e
dsj <1315577081@qq.com>
2025-01-23 12:20:19 +0800

    hot082 打家劫舍

--- hot100/hot082-lc198.cpp
+++ hot100/hot082-lc198.cpp
@@ -6,15 +6,17 @@ class Solution {
   public:
     int rob(vector<int> &nums) {
         int n = nums.size();
+        if (n == 0)
+            return 0;
+        if (n == 1)
+            return nums[0];
         vector<int> dp(n, 0);
-        int ret = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            dp[i] = nums[i];
-            for (int j = 0; j <= i - 2; j++) {
-                dp[i] = max(dp[i], nums[i] + dp[j]);
-            }
-            ret = max(ret, dp[i]);
+        dp[0] = nums[0];
+        dp[1] = max(nums[0], nums[1]);
+        for (int i = 2; i < n; i++) {
+            dp[i] = max(nums[i] + dp[i - 2], dp[i - 1]);
         }
-        return ret;
+
+        return dp[n - 1];
     }
 };
